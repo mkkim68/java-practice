16ccc3175ff27e043ea25ff044a2b5511f5628a9
김민경 <infinitelove367@gmail.com>
2026-03-17 18:52:03 +0900

    sec05/chap05-07

---
+++ src/sec05/chap04/ex03/Product.java
@@ -0,0 +1,33 @@
+package sec05.chap04.ex03;
+
+public class Product {
+
+    private static double discount = 0.2;
+    private static double increaseLimit = 1.2;
+
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isBlank()) return;
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        int max = (int) (this.price * increaseLimit);
+        this.price = price;
+    }
+}
---
+++ src/sec05/chap07/ex01/YalcoChickenHighWayDT.java
@@ -0,0 +1,5 @@
+package sec05.chap07.ex01;
+
+// ⚠️ 불가
+public class YalcoChickenHighWayDT {
+}
